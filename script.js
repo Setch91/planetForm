f5a8ec2828ae342e24844049f53b8c6a33e4fa56
Setch <jsetch91@yahoo.com>
2019-10-11 12:52:18 -0500

    fixed alerts

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ window.addEventListener("load",function(){
       let copilotInput = document.querySelector("input[name=copilotName")
       let fuelInput = document.querySelector("input[name=fuelLevel")
       let cargoInput = document.querySelector("input[name=cargoMass")
-      if (pilotInput === ""|| copilotInput === "" || fuelInput === "" || cargoInput === ""){
+      if (pilotInput.value === ""|| copilotInput.value === "" || fuelInput.value === "" || cargoInput.value === ""){
          alert("All fields are required!")
+         event.preventDefault()
+      } 
+      else if (typeof pilotInput != "string" || typeof copilotInput != "string" || typeof fuelInput != "number" || typeof cargoInput != "number"){
+         alert("Incorrect values!")
          event.preventDefault()
       }
    })
